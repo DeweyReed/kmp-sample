07a29b8bd4528bfc106c6604c56a4152de562b1c
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2025-12-12 16:34:32 +0800

    Remove `Greeting` and `Platform` classes and clean up `App.kt`

--- composeApp/src/commonMain/kotlin/com/github/deweyreed/souvenir/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.github.deweyreed.souvenir
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/jvmMain/kotlin/com/github/deweyreed/souvenir/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.github.deweyreed.souvenir
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
